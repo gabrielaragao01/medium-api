4e044a64310537ef66bb78264ed0bcb989d3873f
gabrielaragao01 <biel.aragao10@gmail.com>
2024-09-17 13:31:24 -0300

    fix: remove unsed coments postRoutes

--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -1,22 +1,9 @@
-// import { Router } from 'express'
-// import postController from '../controllers/PostController'
-
-// const router = new Router()
-
-// router.post('/', postController.create);
-// router.get('/', postController.list);
-// router.get('/:id', postController.listById);
-// router.delete('/', postController.delete);
-// router.put('/',  postController.update);
-
-// export default router;
-
 import BaseRoutes from './baseRoutes.js';
 import PostController from '../controllers/PostController.js';
 // import SchemaValidator from '../utils/SchemaValidator.js';
 // import userSchema from '../schema/userSchema.js';
 
-class UserRoutes extends BaseRoutes {
+class PostRoutes extends BaseRoutes {
 	constructor() {
 		super();
 		this.postController = new PostController();
@@ -28,9 +15,9 @@ class UserRoutes extends BaseRoutes {
 		this.router.delete('/:id', this.postController.delete);
 		this.router.get('/', this.postController.list);
 		this.router.get('/:id', this.postController.listById);
-		
+
 
 		return this.router;
 	}
 }
-export default UserRoutes;
+export default PostRoutes;
